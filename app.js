5a4adb3a7cf78466eabdc78664be41b988c2f9b2
rahul saini <irahulsaininopati@.com>
2025-11-10 11:27:45 +0530

    start 8

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
-function main(){
-    console.log("hello rahul shakya");
-}
+const express=require("express");
+const app=express();
+const ejsmate=require("ejs-mate");
+const path=require("path");
 
-main();
+const port=8080;
+
+app.engine("ejs",ejsmate);
+app.set("view engine", "ejs");
+app.set("views", "./views");
+app.use(express.static("public"))
+
+app.listen(port,function(){
+    console.log("Server is started----");
+})
+app.get("/",function(req,res){
+   res.render("listing/index.ejs");
+
+})
