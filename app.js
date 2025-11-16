ebc48c1fd526936fad6473a33321c41bbe0f3ca9
rahul saini <irahulsaininopati@.com>
2025-11-16 09:52:58 +0530

    final second

--- app.js
+++ app.js
@@ -86,7 +86,7 @@ app.use(express.urlencoded({extended:true}));
 
 
 async function main(){
-    await  mongoose.connect("mongodb://127.0.0.1:27017/myDB");
+    await   mongoose.connect("mongodb://127.0.0.1:27017/myDB");
 }
 
 main().then(function(){
